1ba7e7b0b410d935cac7f33874acf8d3f03440f8
Mike Baldacci <mikebaldacci@MacBookMike2024.local>
2024-11-28 10:05:36 +0100

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      title: "Todolist",
+    };
+  },
+  methods: {},
+});
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <ul>
+    <li>Todo 1</li>
+    <li>Todo 2</li>
+    <li>Todo 3</li>
+  </ul>
+</template>
+
+<style scoped>
+h1 {
+  color: #111;
+}
+</style>
